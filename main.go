ee585eefaab3c0b4dfd3ce29e0a43488c2b85812
Heiser <heiser9295@hotmail.com>
2023-09-08 13:59:49 +0800

    very basic server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world!")
+}
 
 func main() {
-	fmt.Println("hello world!")
+	// servemux == router
+	mux := http.NewServeMux()
+	// register a handler in our servemux (router)
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :7777")
+	err := http.ListenAndServe(":7777", mux)
+	log.Fatal(err)
 }

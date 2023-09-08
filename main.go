eb04acd7de8bbba89e66803857f884b75f9d8672
Heiser <heiser9295@hotmail.com>
2023-09-08 14:14:03 +0800

    handle missing pages

--- main.go
+++ main.go
@@ -9,7 +9,20 @@ import (
 var serveURL string = "localhost:7777"
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world!")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "hello world!")
+		return
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "snippet view")
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "snippet create")
 }
 
 func main() {
@@ -17,6 +30,8 @@ func main() {
 	mux := http.NewServeMux()
 	// register a handler in our servemux (router)
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on " + serveURL)
 	err := http.ListenAndServe(serveURL, mux)

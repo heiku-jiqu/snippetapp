62d55fe2ef01b8368bb4c654166356151fcde567
Heiser <heiser9295@hotmail.com>
2023-09-12 22:26:58 +0800

    cli flag for server address

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-var serveURL string = "localhost:7777"
+const (
+	defaultServeURL    string = "localhost:7777"
+	defaultUiStaticDir string = "./ui/static/"
+)
 
 func main() {
+	addr := flag.String("addr", defaultServeURL, "HTTP network address")
+	flag.Parse() // parse input flags, if not will stay as default vals
+
 	// servemux == router
 	mux := http.NewServeMux()
 	// register a handler in our servemux (router)
@@ -15,10 +22,10 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(defaultUiStaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Print("starting server on " + serveURL)
-	err := http.ListenAndServe(serveURL, mux)
+	log.Printf("starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }

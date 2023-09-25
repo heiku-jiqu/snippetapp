aed05277a1b40c9acb9320bafcc92211ff6a24a1
Heiser <heiser9295@hotmail.com>
2023-09-25 10:46:05 +0800

    validate form inputs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/heiku-jiqu/snippetapp/internal/models"
 	"github.com/julienschmidt/httprouter"
@@ -67,6 +69,27 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	parsedExpires, err := strconv.Atoi(expires)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if parsedExpires != 1 && parsedExpires != 7 && parsedExpires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
 		return
 	}
 

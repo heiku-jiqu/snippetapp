1653f8d1d925621439fedcf3a958b7dba7ff00f8
Heiser <heiser9295@hotmail.com>
2023-09-27 16:51:14 +0800

    add flash message

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,7 +1,5 @@
 var navLinks = document.querySelectorAll("nav a");
-console.log(navLinks);
 navLinks.forEach(node => {
-  console.log(node)
   if (node.getAttribute('href') == window.location.pathname) {
     node.classList.add("live");
   }

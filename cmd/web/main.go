cf32ab2fe7daf331ac3d75ac2da39fe4f6aba359
Heiser <heiser9295@hotmail.com>
2023-10-02 16:14:50 +0800

    add timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -82,10 +82,13 @@ func main() {
 	}
 
 	svr := &http.Server{
-		Addr:      cfg.addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         cfg.addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("starting server on %s", cfg.addr)

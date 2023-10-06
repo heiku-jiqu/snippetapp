aef3a5a1bf89fc7de3057becb29052c6f3e3ca45
Heiser <heiser9295@hotmail.com>
2023-10-06 14:27:16 +0800

    add authorization to nav

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -46,8 +46,9 @@ func (app *application) render(w http.ResponseWriter, status int, templateName s
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -68,4 +69,8 @@ func (app *application) decodePostForm(r *http.Request, destination interface{})
 		return err
 	}
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }

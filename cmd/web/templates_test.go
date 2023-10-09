81e6e2476beab0e5e214f522a6a6d366d7a60b01
Heiser <heiser9295@hotmail.com>
2023-10-09 16:46:41 +0800

    add simple test for humanDate

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	testCases := []struct {
+		testname string
+		input    time.Time
+		expected string
+	}{
+		{
+			"Check humanDate",
+			time.Date(2020, 3, 17, 10, 15, 0, 0, time.UTC),
+			"17 Mar 2020 at 10:15",
+		},
+		{
+			"Check zerotime is empty string",
+			time.Time{},
+			"",
+		},
+		{
+			"Always use UTC timezone",
+			time.Date(2020, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			"17 Mar 2020 at 09:15",
+		},
+	}
+	for _, c := range testCases {
+		t.Run(c.testname, func(t *testing.T) {
+			output := humanDate(c.input)
+			if output != c.expected {
+				t.Errorf("expected %q, but got %q", c.expected, output)
+			}
+		})
+	}
+}
